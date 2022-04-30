3d821115da700ec132fba4e6e88f16154b5357a6
SiveMdluli <Mdlulisive202@gmail.com>
2022-04-30 13:48:34 +0200

    been reading and practicing

---
+++ reactJSbeginner/index.js
@@ -0,0 +1,11 @@
+// var name = "Sive";
+// console.log(name);
+// name = "Stivie";
+// console.log(name);
+
+
+function printName() {
+    var name = "Sive";
+    console.log(name);
+}
+printName();
