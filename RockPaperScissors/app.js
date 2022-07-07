ab23fc92584022d788c5856cfe5e3a66ab497282
SiveMdluli <Mdlulisive202@gmail.com>
2022-07-07 11:49:13 +0200

    Creating a JavaScript 'RPS' game

---
+++ RockPaperScissors/app.js
@@ -0,0 +1,5 @@
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+
+
